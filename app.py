f36e6309f05d110b47a9dc0650b2b0bc3835c150
Yash <yashkumarsaini158@gmail.com>
2026-05-13 12:15:33 +0530

    debuging

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ import json
 import warnings
 import numpy as np
 import pandas as pd
-import joblib
+try:
+    import joblib
+except ImportError:
+    from sklearn.externals import joblib
 import streamlit as st
 import matplotlib.pyplot as plt
 from pathlib import Path
